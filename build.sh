01ee951a56b07ad1be64c7767caacc3a357e0e32
Lior Lerner <liorlerner@outlook.com>
2026-05-09 17:06:17 -0300

    crawler updated

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+pip install -r requirements.txt
+wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
--- crawler.py
+++ crawler.py
@@ -27,14 +27,18 @@ def human_scroll(driver):
 
 def open_company_page(target_company: str):
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--log-level=3")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.binary_location = "/usr/bin/google-chrome"
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
